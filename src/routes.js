03aa262bf3fd2849f07143ce73bb78d79dcdb4ef
Lemik <lemik@i.ua>
2017-04-28 08:11:40 -0700

    MAnageCoinsPage

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,15 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import AddCoinPage from './components/Coin/CoinPage';
+import AddCoinPage from './components/coin/CoinPage';
+import ManageCoinPage from './components/coin/ManageCoinPage'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
-    <Route path="coin" component={AddCoinPage} />
+    <Route path="coins" component={AddCoinPage} />
+    <Route path="coins/:id" component={AddCoinPage} />
+    <Route path="coin" component={ManageCoinPage} />
   </Route>
 );
